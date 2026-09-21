43fcf358a3beac8124ad0b367334449f84366ff5
biancamargarettevallo <biancamargarette.vallo@cit.edu>
2026-09-21 17:18:23 +0800

    1.1: course information step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p>{props.part3} {props.exercises3}</p>
+  </div>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+)
+
 const App = () => {
+  const course = 'CSIT340 - Industry Elective 1'
+  const part1 = 'CSIT341 - Industry Elective 2'
+  const exercises1 = 3
+  const part2 = 'CSIT342 - Industry Elective 3'
+  const exercises2 = 3
+  const part3 = 'CSIT440 - Industry Trends'
+  const exercises3 = 3
+
   return (
     <div>
-      <p>Hello world</p>
+      <Header course={course} />
+      <Content
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
